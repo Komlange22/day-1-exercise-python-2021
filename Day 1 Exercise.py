19e342d25b1ba8b59bd9e4984e77b39ded069fb8
Komlange22 <87339779+Komlange22@users.noreply.github.com>
2021-07-14 20:50:15 -0700

    Add files via upload

---
+++ Day 1 Exercise.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Decode this secrete message:
+# 01001100 01101111 01100111 01101001 01100011
+
+# 76 110 102 104 98
+
+# Type Markdown and LaTeX:  𝛼2
+
+# 2. Identify and explain stages of Programming.
+
+# In[ ]:
+
+
+Analyze the problem, plan,code, testing, maintenance
+
+
+# Type Markdown and LaTeX:  𝛼2
+
+# 3. Explain why do we use Selection? What kind of Statements or code do we use with that?
+
+# To select the right code of the task. the code used are true or false
+
+# Type Markdown and LaTeX:  𝛼2
+
+# 4. Explain why do we use Sequence? Why is this part integral?
+
+# we use sequence to put in order the outcome of the process.
+
+# Type Markdown and LaTeX:  𝛼2
+
+# 5. Explain why do we use Repetition? What kind of statements or code do we use with that?
+
+# It's used to repeat the same code every time. true or false are the code.
+
+# Type Markdown and LaTeX:  𝛼2
+
+# 6. Knowing all this, explain why do we use the Structured Program Theorom and why do you need to know it?
+
+# SPT has 3 components whitch are 3 small pieces of tasks used to perform a large task
+
+# Type Markdown and LaTeX:  𝛼2
